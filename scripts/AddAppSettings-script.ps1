fbb81d8039be5bf85a0343d06f8bdbefb4f0aad0
phanikumarsharma <ramaduguphanikumar@gmail.com>
2018-09-18 00:54:48 -0700

    script committed

---
+++ scripts/AddAppSettings-script.ps1
@@ -0,0 +1,8 @@
+﻿    
+Write-Output "Adding App settings to ApiApp"
+$ApiAppSettings = @{"AppSetting1" = "$AppSetting1";
+                    "AppSetting2" = "$AppSetting2";
+                    "AppSetting3" = "$AppSetting3"
+                    }
+
+Set-AzureRmWebApp -AppSettings $ApiAppSettings -Name $ApiApp -ResourceGroupName $ResourceGroupName
