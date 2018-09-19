e0527773a31ede01390b3164f2be5bd0dd3e6c85
phanikumarsharma <ramaduguphanikumar@gmail.com>
2018-09-18 23:11:34 -0700

    azuredeploy.json file committed

--- scripts/AddAppSettings-script.ps1
+++
@@ -1,8 +0,0 @@
-﻿    
-Write-Output "Adding App settings to ApiApp"
-$ApiAppSettings = @{"AppSetting1" = "$AppSetting1";
-                    "AppSetting2" = "$AppSetting2";
-                    "AppSetting3" = "$AppSetting3"
-                    }
-
-Set-AzureRmWebApp -AppSettings $ApiAppSettings -Name $ApiApp -ResourceGroupName $ResourceGroupName
